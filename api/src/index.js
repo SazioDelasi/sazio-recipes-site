e284e9f502e6b8b5d8e643bcd964a8892208c37f
SazioDelasi <161116134+SazioDelasi@users.noreply.github.com>
2025-08-27 18:36:15 +0000

    Update index.js

--- api/src/index.js
+++ api/src/index.js
@@ -5,4 +5,4 @@ const app = require('./app.js');
 const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server is on at http://${HOST}:${PORT}`));
+app.listen(PORT, () => console.log(`Server is on at http://${HOST}:${PORT}`))
