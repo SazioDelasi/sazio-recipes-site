f915c4e17eb5e603272a0f71a4f811089a2871ed
Bayaan <65360444+bayaan-nasir@users.noreply.github.com>
2025-08-27 16:04:09 +0000

    Create app.js

---
+++ api/src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const foodRoutes = require("./routes/food-routes.js");
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/food", foodRoutes);
+
+app.get("/", (req, res) => {
+	res.send("Welcome to the Recipes API");
+});
+
+module.exports = app;
